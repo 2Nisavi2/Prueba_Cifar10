4fadb9adfde0d83c6e3b677382ad9e3d579a7c81
Nicolás Ávila <107935684+2Nisavi2@users.noreply.github.com>
2023-07-09 20:31:00 -0500

    Subida archivos

---
+++ jscript/target_classes.js
@@ -0,0 +1,15 @@
+
+// Asociación de Clases con su etiqueta, tenga en cuenta que es tipo diccionario, key: value,
+
+TARGET_CLASSES = {
+    0: 'Avion',
+    1: 'Auto',
+    2: 'Ave',
+    3: 'Gato',
+    4: 'Ciervo',
+    5: 'Perro',
+    6: 'Rana',
+    7: 'Caballo',
+    8: 'Obeja',
+    9: 'Camión'
+};
